7272c3fb192beb21f840f1dd3c43b5b3e1463406
yanic <yanic.dev@gmail.com>
2025-12-02 20:42:30 +0100

    add more lsp-servers

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,4 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "lua_ls", "qmlls", "hyprls" }
+local servers = { "html", "cssls", "lua_ls", "qmlls", "hyprls", "eslint", "tailwindcss" }
 vim.lsp.enable(servers)
